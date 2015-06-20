485c4283073836459c6f8474f534233ae0dc1d22
Adam Tengler <a.tengler@tcpcloud.eu>
2015-06-21 00:15:02 +0200

    Comment fix

--- learning_assignments/coin_toss.py
+++ learning_assignments/coin_toss.py
@@ -6,9 +6,9 @@ trials = 10000
 flips = 0
 
 """Average number of coin tosses needed to get different result than we got at first toss. 
-For example when first coin toss results in heads and secon tails, there were two coin tosses needed.
+For example when first coin toss results in heads and second in tails, there were two coin tosses needed.
 If first coin toss results in tails, second in tails and third in heads, we needed three coin 
-tosses to get different result."""
+tosses to get different result etc."""
 
 for trial in range(1, trials):
     first_flip = randint(0, 1)
