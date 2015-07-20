b376546f1a327c708e4fa96a17a3228ad3f3fc5e
Adam Tengler <a.tengler@tcpcloud.eu>
2015-07-20 22:14:26 +0200

    Config file for flasktaskr added

---
+++ learning_assignments_2/flasktaskr/flasktaskr/project/_config.py
@@ -0,0 +1,13 @@
+import os
+
+# grab the folder where this script lives
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+DATABASE = 'flasktaskr.db'
+USERNAME = 'admin'
+PASSWORD = 'admin'
+WTF_CSRF_ENABLED = True
+SECRET_KEY = 'ZjM5ZTI4OGM1YTYyNGIyNDNhZDA4M2Y2'
+
+# define the full path for the database
+DATABASE_PATH = os.path.join(basedir, DATABASE)
