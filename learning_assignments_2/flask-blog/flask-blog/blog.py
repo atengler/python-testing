3d7feea4e712f829fddf12d4b8e9a5875215420b
Adam Tengler <a.tengler@tcpcloud.eu>
2015-07-20 08:30:06 +0200

    Flask blog initial commit

---
+++ learning_assignments_2/flask-blog/flask-blog/blog.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template, request, session, flash, redirect, url_for, g
+import sqlite3
+
+# configuration
+DATABASE = 'blog.db'
+USERNAME = 'admin'
+PASSWORD = 'admin'
+SECRET_KEY = 'NWRmN2U3ZmVhNzYyMTFjMWE0Njc0NDFm'
+
+app = Flask(__name__)
+
+app.config.from_object(__name__)
+
+def connect_db():
+  return sqlite3.connect(app.config['DATABASE_PATH'])
+
+@app.route('/')
+def login():
+  return render_template('login.html')
+
+@app.route('/main')
+def main():
+  return render_template('main.html')
+
+if __name__ == '__main__':
+  app.run(debug=True)
