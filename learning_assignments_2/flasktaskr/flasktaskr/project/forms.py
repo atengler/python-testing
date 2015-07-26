5ae2bd3fe0b629a40b73bb07c14fd31d4ab83499
Adam Tengler <a.tengler@tcpcloud.eu>
2015-07-26 02:06:12 +0200

    Tests added + PEP8 formatting

--- learning_assignments_2/flasktaskr/flasktaskr/project/forms.py
+++ learning_assignments_2/flasktaskr/flasktaskr/project/forms.py
@@ -1,50 +1,52 @@
 from flask_wtf import Form
 from wtforms import StringField, DateField, IntegerField, SelectField, PasswordField
-from wtforms.validators import DataRequired, Length, EqualTo
+from wtforms.validators import DataRequired, Length, EqualTo, Email
 
 
 class AddTaskForm(Form):
-  task_id = IntegerField()
-  name = StringField('Task Name', validators=[DataRequired()])
-  due_date = DateField(
-    'Due Date (mm/dd/yyyy)',
-    validators = [DataRequired()], format='%m/%d/%Y'
-  )
-  priority = SelectField(
-    'Priority',
-    validators = [DataRequired()],
-    choices = [
-      ('1', '1'), ('2', '2'), ('3', '3'), ('4', '4'), ('5', '5'), ('6', '6'), ('7', '7'), ('8', '8'), ('9', '9'), ('10', '10')
-    ]
-  )
-  status = 1
+    task_id = IntegerField()
+    name = StringField('Task Name', validators=[DataRequired()])
+    due_date = DateField(
+        'Due Date (mm/dd/yyyy)',
+        validators=[DataRequired()], format='%m/%d/%Y'
+    )
+    priority = SelectField(
+        'Priority',
+        validators=[DataRequired()],
+        choices=[
+            ('1', '1'), ('2', '2'), ('3', '3'), ('4', '4'), ('5', '5'), ('6',
+                                                                         '6'), ('7', '7'), ('8', '8'), ('9', '9'), ('10', '10')
+        ]
+    )
+    status = 1
 
 
 class RegisterForm(Form):
-  name = StringField(
-    'Username',
-    validators = [DataRequired(), Length(min=5, max=25)]
-  )
-  email = StringField(
-    'Email',
-    validators = [DataRequired(), Length(min=5, max=40)]
-  )
-  password = PasswordField(
-    'Password',
-    validators = [DataRequired(), Length(min=5, max=40)]
-  )
-  confirm = PasswordField(
-    'Repeat Password',
-    validators = [DataRequired(), EqualTo('password', message = 'Passwords must match')]
-  )
+    name = StringField(
+        'Username',
+        validators=[DataRequired(), Length(min=5, max=25)]
+    )
+    email = StringField(
+        'Email',
+        validators=[DataRequired(), Email(), Length(min=5, max=40)]
+    )
+    password = PasswordField(
+        'Password',
+        validators=[DataRequired(), Length(min=5, max=40)]
+    )
+    confirm = PasswordField(
+        'Repeat Password',
+        validators=[
+            DataRequired(), EqualTo('password', message='Passwords must match')]
+    )
 
 
 class LoginForm(Form):
-  name = StringField(
-    'Username',
-    validators = [DataRequired()]
-  )
-  password = PasswordField(
-    'Password',
-    validators = [DataRequired()]
-  )
+    name = StringField(
+        'Username',
+        validators=[DataRequired()]
+    )
+    password = PasswordField(
+        'Password',
+        validators=[DataRequired()]
+    )
